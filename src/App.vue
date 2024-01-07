0682deb9ee794a0e6e6ee558b8b5fb295716b855
C. Rensberry <gitberry@northberry.ca>
2024-01-06 23:31:31 -0600

    fuller dataset and display width tweaks

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import { format } from 'date-fns'
 import EditButton from '@/components/EditButton.vue'
 import MetaButton from '@/components/MetaButton.vue'
+import DisplayTrunc from '@/components/DisplayTrunc.vue'
 
 import TableTanstack from '@/components/TableTanstack.vue'
 
@@ -13,20 +14,28 @@ const columnsLibraryIndex = [
 //{ accessorKey: 'id'                    , header: 'id'                    , enableSorting:false },
   { accessorKey: 'Edit'                  , header:  '',
     cell: ({ row }) => h(EditButton, { id: row.original.id })              , enableSorting:false },
-  { accessorKey: 'Title'                 , header: 'Title'                 , enableSorting:true },
-  { accessorKey: 'SubTitle'              , header: 'SubTitle'              , enableSorting:false },
-  { accessorKey: 'Composer'              , header: 'Composer'              , enableSorting:false },
+//{ accessorKey: 'Title'                 , header: 'Title'                 , enableSorting:true },
+  { accessorKey: 'Title', 
+     header: 'Piece',
+     cell: ({ row }) => h(DisplayTrunc, {
+      displaydata: row.original.Title, 
+      metadata: row.original.Title,
+      dispmax: 33,
+      }), 
+      enableSorting:true },
+//{ accessorKey: 'SubTitle'              , header: 'SubTitle'              , enableSorting:false },
+  { accessorKey: 'Composer'              , header: 'Composer'              , enableSorting:true },
 //{ accessorKey: 'ComposerPickerID'      , header: 'ComposerPickerID'      , enableSorting:false },
-  { accessorKey: 'Lyricist'              , header: 'Lyricist'              , enableSorting:false },
-  { accessorKey: 'Arranger'              , header: 'Arranger'              , enableSorting:false },
-  { accessorKey: 'Publisher'             , header: 'Publisher'             , enableSorting:false },
+//{ accessorKey: 'Lyricist'              , header: 'Lyricist'              , enableSorting:false },
+//{ accessorKey: 'Arranger'              , header: 'Arranger'              , enableSorting:false },
+  { accessorKey: 'Publisher'             , header: 'Publisher'             , enableSorting:true },
 //{ accessorKey: 'CopyrightNumber'       , header: 'CopyrightNumber'       , enableSorting:false },
-  { accessorKey: 'UserPermissionIfNotFGS', header: 'Legal', enableSorting:false },
-  { accessorKey: 'PricePerCopy'          , header: 'PricePerCopy'          , enableSorting:false },
+//{ accessorKey: 'UserPermissionIfNotFGS', header: 'Legal', enableSorting:false },
+//{ accessorKey: 'PricePerCopy'          , header: 'PricePerCopy'          , enableSorting:false },
 //{ accessorKey: 'PricePerCopyAsOf'      , header: 'PricePerCopyAsOf'      , enableSorting:false },
-  { accessorKey: 'Style'                 , header: 'Style'                 , enableSorting:false },
-  { accessorKey: 'Instrumentation'       , header: 'Instrumentation'       , enableSorting:false },
-  { accessorKey: 'Level'                 , header: 'Level'                 , enableSorting:false },
+  { accessorKey: 'Style'                 , header: 'Style'                 , enableSorting:true },
+//{ accessorKey: 'Instrumentation'       , header: 'Instrumentation'       , enableSorting:false },
+//{ accessorKey: 'Level'                 , header: 'Level'                 , enableSorting:false },
 //{ accessorKey: 'ModifiedBy'            , header: 'ModifiedBy'            , enableSorting:false },
 //{ accessorKey: 'ModifiedByUserId'      , header: 'ModifiedByUserId'      , enableSorting:false },
 //{ accessorKey: 'ModifiedOn'            , header: 'Edited'                ,
@@ -43,9 +52,10 @@ const columnsLibraryIndex = [
        ' By:' + row.original.CreatedBy +
        '\nEdited:' + format(new Date(row.original.ModifiedOn), 'MMM d, yyyy HH:mm:ss') + 
        ' By:' + row.original.ModifiedBy +
-       '\nIn your timezone (UTC' + new Date().getTimezoneOffset()*-1/60 +' )'
+       '\nIn your timezone (UTC' + new Date().getTimezoneOffset()*-1/60 +' )',
+       metaindicator: '&nbsp;&#9432;'
       }), 
-      enableSorting:false },
+      enableSorting:true },
 //{ accessorKey: 'CreatedBy'             , header: 'CreatedBy'             , enableSorting:false },
 //{ accessorKey: 'CreatedById'           , header: 'CreatedById'           , enableSorting:false },
 //{ accessorKey: 'CreatedOn'             , header: 'CreatedOn'             , enableSorting:false },
--- src/components/DisplayTrunc.vue
+++ src/components/DisplayTrunc.vue
@@ -4,10 +4,15 @@ const showit = (msgg) => {
   console.log(msgg); //todo: remove this after debugging
   alert(msgg);
 };
+const truncit = (msgg,msggmax) => {
+  if (msgg.length > msggmax ) msgg = msgg.substring(0,msggmax) + ' ...';
+  return msgg;
+}
 
 const props = defineProps({
   displaydata: String,
-  metadata: String,  
+  metadata: String, 
+  dispmax: Number, 
 });
 </script>
 
@@ -16,6 +21,6 @@ const props = defineProps({
   class="text-indigo-600 hover:text-indigo-500" 
   v-on:click=showit(metadata);
   >
-  {{ displaydata }}&nbsp;&#9432;
+  {{ truncit(displaydata,dispmax) }}
   </span>
 </template>
